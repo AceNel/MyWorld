d15d6e5a24e62ffe9787fb1e2e2fcd0b1e6f9036
AceNel <Jamara254>
2019-12-17 08:26:48 +0300

    Implemented butter Knife to the project

---
+++ app/src/main/java/com/example/myworld/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.myworld;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import android.support.v7.app.AppCompatActivity;
+
+import java.lang.reflect.Type;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.bt_click_me) Button mFindRestaurantsButton;
+    @BindView(R.id.textView3) TextView mAppNameTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface caviarFont = Typeface.createFromAsset(getAssets(), "fonts/CaviarDreams.ttf");
+        mAppNameTextView.setTypeface(caviarFont);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, AfricaActivity.class);
+                // String location = mLocationEditText.getText().toString();
+                // intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
+    }
+}
