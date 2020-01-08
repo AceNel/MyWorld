a96a95ce7de90bd43db490ac71710a5f6bd54722
AceNel <Jamara254>
2020-01-08 10:06:41 +0300

    Added the main activity UI

---
+++ app/src/main/java/com/example/myworld/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    Button europe;
+    Button africa;
+    Button americas;
+    Button asia;
+    Button oceanie;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        europe = (Button) findViewById(R.id.Europe);
+        africa = (Button) findViewById(R.id.Africa);
+        americas = (Button) findViewById(R.id.Americas);
+        asia = (Button) findViewById(R.id.Asia);
+        oceanie = (Button) findViewById(R.id.Oceania);
+
+        europe.setOnClickListener(this.OnClickListener(europe.getText().toString()));
+        africa.setOnClickListener(this.OnClickListener(africa.getText().toString()));
+        americas.setOnClickListener(this.OnClickListener(americas.getText().toString()));
+        asia.setOnClickListener(this.OnClickListener(asia.getText().toString()));
+        oceanie.setOnClickListener(this.OnClickListener(oceanie.getText().toString()));
+    }
+
+    private View.OnClickListener OnClickListener(final String region) {
+        return new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ListCountry.class);
+                intent.putExtra("region", region);
+                startActivity(intent);
+            }
+        };
+    }
+
+}
