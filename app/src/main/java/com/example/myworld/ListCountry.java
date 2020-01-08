c322987ef36f98471714904baf05af0181af7a17
AceNel <Jamara254>
2020-01-08 10:05:14 +0300

    Added the countries list UI

---
+++ app/src/main/java/com/example/myworld/ListCountry.java
@@ -0,0 +1,94 @@
+package com.example.myworld;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+
+
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class ListCountry extends AppCompatActivity {
+
+    ArrayAdapter adapter;
+    Map<String, ArrayList<Country.CountryType>> response;
+    ListView listView;
+    String continent;
+    Country country;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_country);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        Intent intent = getIntent();
+        continent = intent.getStringExtra("region");
+
+        try {
+            if(networkAvailable()) {
+                country = new Country();
+                response = country.run(continent);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (response != null) {
+            ArrayList<String> countryList  = new ArrayList<>();
+            for (Country.CountryType item : response.get(continent)) {
+                countryList.add(item.name + " - " + item.capital);
+            }
+
+            adapter = new ArrayAdapter<>(this, R.layout.activity_list_country,
+                    R.id.listCountryTextView, countryList);
+            if (listView != null) {
+                listView.setAdapter(adapter);
+            }
+        } else {
+            System.out.println("ERROR ON GET REPONSE");
+        }
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), ViewCountry.class);
+                intent.putExtra("countryName", response.get(continent).get(position).name);
+                intent.putExtra("countryRegion", response.get(continent).get(position).region);
+                intent.putExtra("countryPopulation", response.get(continent).get(position).population);
+                intent.putExtra("countryCapital", response.get(continent).get(position).capital);
+                intent.putExtra("countryFlag", response.get(continent).get(position).flag);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private boolean networkAvailable()
+    {
+        boolean network = false;
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            network = true;
+        }
+
+        return network;
+    }
+
+
+}
